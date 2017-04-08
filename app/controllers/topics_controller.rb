dd24911f2516c24934ced182290460a3ddde6efd
Drew Mace <dremac1@live.com>
2017-04-08 04:39:35 +0000

    votes added

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -29,41 +29,41 @@ class TopicsController < ApplicationController
     @topic = Topic.find(params[:id])
   end
   
-   def update
-      @topic = Topic.find(params[:id])
-      @topic.assign_attributes(topic_params)
+  def update
+    @topic = Topic.find(params[:id])
+    @topic.assign_attributes(topic_params)
  
-     if @topic.save
-        flash[:notice] = "Topic was updated."
-       redirect_to @topic
-     else
-       flash.now[:alert] = "Error saving topic. Please try again."
-       render :edit
-     end
-   end
+    if @topic.save
+      flash[:notice] = "Topic was updated."
+      redirect_to @topic
+    else
+      flash.now[:alert] = "Error saving topic. Please try again."
+      render :edit
+    end
+  end
    
-   def destroy
-     @topic = Topic.find(params[:id])
+  def destroy
+    @topic = Topic.find(params[:id])
  
-     if @topic.destroy
-       flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
-       redirect_to action: :index
-     else
-       flash.now[:alert] = "There was an error deleting the topic."
-       render :show
-     end
-   end
+    if @topic.destroy
+      flash[:notice] = "\"#{@topic.name}\" was deleted successfully."
+      redirect_to action: :index
+    else
+      flash.now[:alert] = "There was an error deleting the topic."
+      render :show
+    end
+  end
    
-   private
-   def topic_params
-     params.require(:topic).permit(:name, :description, :public)
-   end
+  private
+  def topic_params
+    params.require(:topic).permit(:name, :description, :public)
+  end
    
-   def authorize_user
-     unless current_user.admin?
-       flash[:alert] = "You must be an admin to do that."
-       redirect_to topics_path
-     end
-   end
+  def authorize_user
+    unless current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to topics_path
+    end
+  end
 end
    
--- config/routes.rb
+++ config/routes.rb
@@ -6,16 +6,21 @@ Rails.application.routes.draw do
   
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+    
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
   
-  resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   post 'users/confirm' => 'users#confirm'
   
   
   
   resources :questions
   resources :advertisements
+  
+  
 
   get 'about' => 'welcome#about'
   
